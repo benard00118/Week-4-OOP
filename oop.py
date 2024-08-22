a37d8f66ba3e84e5d30af95487e0ba8ecc57bda5
Benard Karanja <benardopeter4@gmail.com>
2024-08-22 09:08:26 +0300

    first commit

---
+++ oop.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I am {self.gender}.")
+
+
+person_instance = Person("Benard Peter", 20, "Male")
+
+person_instance.introduce()
